182aadb574af262e713fa815ab96a0ddc9a02b33
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-06-19 03:21:56 +0300

    Fix testing errors in travis

--- tests/test_rides.py
+++ tests/test_rides.py
@@ -1,7 +1,6 @@
 import unittest
 import sys  # fix import errors
 import os
-import json
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from tests.base_config_tests import ConfigTestCase
 
