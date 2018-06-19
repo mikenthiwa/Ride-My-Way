248010b4c5f620aef6d800ee5ed349b238967c33
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-06-19 03:14:00 +0300

    Renaming base test folder

--- tests/test_drivers.py
+++ tests/test_drivers.py
@@ -5,7 +5,7 @@ import sys  # fix import errors
 import os
 import json
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from tests.base_config import ConfigTestCase
+from tests.base_config_tests import ConfigTestCase
 
 class DriversEndpoint(ConfigTestCase):
     """This class represents driver test cases"""
--- tests/test_rides.py
+++ tests/test_rides.py
@@ -3,7 +3,7 @@ import sys  # fix import errors
 import os
 import json
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from tests.base_config import ConfigTestCase
+from tests.base_config_tests import ConfigTestCase
 
 class RidesEndpoint(ConfigTestCase):
     """This class represents rides test cases"""
