c2cc8b0b7ef761e1bb2c2f43ae631493113c8a73
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-06-21 08:28:14 +0300

    Changing api documentation path

--- app/__init__.py
+++ app/__init__.py
@@ -6,27 +6,25 @@ from instance.config import app_config
 
 def create_app(config_name):
     # Expect token in api_doc
-
     authorizations = {'apikey': {'type': 'apiKey', 'in': 'header', 'name': 'x-access-token'}}
 
     # Create flask app
-
     app = Flask(__name__, instance_relative_config=True)
-    # Enable swagger editor
+
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    app.config['SWAGGER_UI_JSONEDITOR'] = True
     app.url_map.strict_slashes = False
-
-
 
     api = Api(app=app,
               title='Ride_My_Way',
               authorizations=authorizations,
               version='1.0',
+              doc='/api/v1/documentation',
               description='Ride-My-Way is a carpooling application that provides'
                           ' drivers with the ability to create ride'
                           ' offers and passengers to join available ride offers.')
+    # Enable swagger editor
+    app.config['SWAGGER_UI_JSONEDITOR'] = True
 
     from resources.rides import api as rides
     api.add_namespace(rides, path='/api/v1')
