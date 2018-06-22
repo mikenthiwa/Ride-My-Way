8253916dcb92ac5bf9942dab3193908b48123fdc
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-06-22 07:24:13 +0300

    deploying

--- run.py
+++ run.py
@@ -5,10 +5,9 @@ from app import create_app
 
 config_name = os.getenv("APP_SETTINGS")
 app = create_app(config_name)
-
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
-    app.run()
+    app.run(host="0.0.0.0", port=port)
+    # app.run()
